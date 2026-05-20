3e545c25dbd7b8b2b28e13906e78a23c46bd2bdb
dillon-goknit <dillon.jacob2@gmail.com>
2026-05-20 18:55:37 -0400

    Deploy static site to GitHub Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,12 @@ from extract_title import extract_title
 from markdown_to_html import markdown_to_html_node
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(
+    from_path: str,
+    template_path: str,
+    dest_path: str,
+    basepath: str = "/",
+) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as f:
@@ -17,6 +22,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     title = extract_title(markdown)
 
     page = template.replace("{{ Title }}", title).replace("{{ Content }}", content_html)
+    page = page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_dir):
@@ -27,7 +33,10 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
 
 def generate_pages_recursive(
-    dir_path_content: str, template_path: str, dest_dir_path: str
+    dir_path_content: str,
+    template_path: str,
+    dest_dir_path: str,
+    basepath: str = "/",
 ) -> None:
     for entry in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, entry)
@@ -35,6 +44,6 @@ def generate_pages_recursive(
         if os.path.isfile(src_path):
             if src_path.endswith(".md"):
                 html_dst = dst_path[:-3] + ".html"
-                generate_page(src_path, template_path, html_dst)
+                generate_page(src_path, template_path, html_dst, basepath)
         else:
-            generate_pages_recursive(src_path, template_path, dst_path)
+            generate_pages_recursive(src_path, template_path, dst_path, basepath)
